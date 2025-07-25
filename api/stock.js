75d9b22680cab9540864fee031c107fcba1d734b
yez-park <126441740+yez-park@users.noreply.github.com>
2025-07-25 22:26:03 +0900

    Update stock.js

--- api/stock.js
+++ api/stock.js
@@ -1,3 +1,5 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "Hello from stock.js" });
+export default async function handler(req, res) {
+  const response = await fetch("https://api.stock.naver.com/stockDOM?code=005930"); // 네이버 주식 예시
+  const data = await response.json();
+  res.status(200).json(data);
 }
