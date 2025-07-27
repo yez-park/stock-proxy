be5dabfc181f37bba4509710e669366ffb6c492c
yez-park <126441740+yez-park@users.noreply.github.com>
2025-07-27 12:05:59 +0900

    Update stock.js

--- api/stock.js
+++ api/stock.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
